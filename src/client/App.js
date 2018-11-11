1b45e8b05c5332d4a4220c9fbb4466fe6c7ffbe8
kwality <kwalitycontrol@gmail.com>
2018-11-11 16:03:00 +0900

    initial commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,145 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './app.css';
-import ReactImage from './react.png';
+const renderer = new marked.Renderer();
+
+const createMarkdown = (value) =>{
+  //This makes links open into a new tab
+  renderer.link = (href, title, text) => {
+    return '<a target="_blank" href="'+ href + '" title="' + title +'"">' + text +'</a>'
+  }
+  renderer.br = () => {
+    return "<br>"
+  }
+  return {__html: marked(value, {santize: true, renderer: renderer})}
+}
+
+const TextField = ({value}) => {
+  return (
+      <div>
+        <div className="well" id="preview" dangerouslySetInnerHTML={createMarkdown(value)}></div>
+      </div>
+    )
+}
+
+const TextEditor = ({value, onChange}) =>{
+  return (
+        <div>
+            <textarea rows="25"className="form-control" id="editor" placeholder="Enter your Markdown" value={value} onChange={onChange}></textarea>
+        </div>
+    )
+}
+
 
 export default class App extends Component {
-  state = { username: null };
+  constructor(props){
+    super(props);
+    this.state = {
+      value: testerText,
+      showModal: false
+    }
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handlePreviewFocus = this.handlePreviewFocus.bind(this);
+    this.handleEditorFocus = this.handleEditorFocus.bind(this);
+
+  }
+  handleChange(event){
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  handleClear(event){
+    this.setState({
+      value: ''
+    });
+  }
+
+  handlePreviewFocus(event) {
+    this.setState({
+      modal: true
+    })
+
+  }
+
+  handleEditorFocus(event){
+
   }
 
   render() {
-    const { username } = this.state;
+    const {value} = this.state
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">Markdown Previewer</a>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-6">
+              <button type="button" className="btn btn-danger pull-right clearBtn" onClick={this.handleClear}>Clear</button>
+              <TextEditor value={value} onChange={this.handleChange}/>
+            </div>
+            <div className="col-xs-6">
+              <TextField value={value} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const testerText = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+
